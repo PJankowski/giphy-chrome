ef0f96614ca6c90ebe09aabdfc05e7d144ebf7ab
Paul Jankowski <PJankowski25@gmail.com>
2016-10-13 16:04:15 -0400

    Updated to check for http/https

--- src/index.js
+++ src/index.js
@@ -7,9 +7,33 @@ let images = document.getElementsByTagName('img')
 for (let i=0;i<images.length;i++) {
     axios.get('https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=chuck+norris')
       .then((response) => {
-        images[i].src = response.data.data.image_original_url
+        let source = response.data.data.image_original_url
+
+        if (window.location.protocol == "https:") {
+          let newSource = source.replace('http:', 'https:')
+          images[i].src = newSource
+        } else {
+          images[i].src = source
+        }
       })
       .catch((err) => {
         console.log(err)
       })
 }
+
+images.map((image) => {
+  axios.get('https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=chuck+norris')
+    .then((response) => {
+      let source = response.data.data.image_original_url
+
+      if (window.location.protocol == "https:") {
+        let newSource = source.replace('http:', 'https:')
+        image.src = newSource
+      } else {
+        image.src = source
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
